6898dee164d712fe3c5c6e82a59280e1ca3ec217
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2023-05-16 23:13:07 -0300

    11

--- migrations/Version20220821180851.php
+++ migrations/Version20220821180851.php
@@ -10,22 +10,24 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220819031354 extends AbstractMigration
+final class Version20220821180851 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Criação da tabela de usuários';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE episode ADD COLUMN watched BOOLEAN DEFAULT FALSE;');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE episode DROP COLUMN watched');
+        $this->addSql('DROP TABLE user');
     }
 }
