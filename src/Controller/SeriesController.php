1464c6834f35502414086aef7d72b93eee0c6aad
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2023-05-23 10:59:20 -0300

    15

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -3,21 +3,16 @@
 namespace App\Controller;
 
 use App\DTO\SeriesCreateFromInput;
-use App\Entity\Episode;
-use App\Entity\Season;
 use App\Entity\Series;
 use App\Form\SeriesType;
+use App\Message\SeriesWasCreated;
 use App\Repository\SeriesRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SeriesController extends AbstractController
@@ -25,7 +20,7 @@ class SeriesController extends AbstractController
     public function __construct(
         private SeriesRepository $seriesRepository,
         private EntityManagerInterface $entityManager,
-        private MailerInterface $mailer,
+        private MessageBusInterface $messenger
     )
     {
     }
@@ -57,18 +52,9 @@ class SeriesController extends AbstractController
         if (!$seriesForm->isValid()) {
             return $this->renderForm('series/form.html.twig', compact('seriesForm'));
         }
-        $user = $this->getUser();
 
         $series = $this->seriesRepository->add($input);
-        $email = (new TemplatedEmail())
-            ->from('sistema@example.com')
-            ->to($user->getUserIdentifier())
-            ->subject('Nova série criada')
-            ->text("Série {$series->getName()} foi criada criada")
-            ->htmlTemplate('emails/series-created.html.twig')
-            ->context(compact('series'));
-
-        $this->mailer->send($email);
+        $this->messenger->dispatch(new SeriesWasCreated($series));
 
         $this->addFlash(
             'success',
